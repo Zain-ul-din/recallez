dfcb11e44c82955da66255fa75126baf7f1a4979
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-09-06 20:52:10 +0500

    add test with user id's

--- src/test/db.spec.ts
+++ src/test/db.spec.ts
@@ -43,7 +43,7 @@ describe("Neo4j Tests", () => {
 
       const res = await session.run(query, {
         name: "Github",
-        url: "https://www.github.com",
+        url: "https://www.github.com"
       });
 
       const properties = res.records.map((record) => record.get(0).properties);
@@ -63,7 +63,7 @@ describe("Neo4j Tests", () => {
       const sites = [
         { name: "Github", url: "https://www.github.com" },
         { name: "Stack Overflow", url: "https://stackoverflow.com" },
-        { name: "MDN", url: "https://developer.mozilla.org" },
+        { name: "MDN", url: "https://developer.mozilla.org" }
       ];
 
       const res = await session.run(query, { sites });
@@ -92,7 +92,7 @@ describe("Neo4j Tests", () => {
       const newName = "Github - Home";
       const res = await session.run(query, {
         name: newName,
-        url: "https://www.github.com",
+        url: "https://www.github.com"
       });
 
       const properties = res.records.map((record) => record.get(0).properties);
@@ -107,12 +107,110 @@ describe("Neo4j Tests", () => {
       `;
 
       const searchRes = await session.run(searchQuery, {
-        url: "https://www.github.com",
+        url: "https://www.github.com"
       });
 
       const searchProps = searchRes.records.map(
         (record) => record.get(0).properties
       );
+
+      expect(searchProps.length).toBe(1);
+      expect(searchProps[0].name).toBe(newName);
+    });
+  });
+
+  describe("Idempotent Queries Using User Id", () => {
+    const userId = "LNSCRvfufEYUHxF+LhpDpisAxSIZyplp";
+
+    it("should clear the database", async () => {
+      const res = await session.run("MATCH (n) DETACH DELETE n RETURN n;");
+      expect(res.records.length === 0);
+    });
+
+    it("should create single bookmark node", async () => {
+      const query = `
+        MERGE (s:Site { url: $url, userId: $userId }) 
+        SET s = { name: $name, url: $url, userId: $userId }
+        RETURN s;
+      `;
+
+      const res = await session.run(query, {
+        name: "Github",
+        url: "https://www.github.com",
+        userId
+      });
+
+      const properties = res.records.map((record) => record.get(0).properties);
+      expect(properties.length === 1, "should return one item");
+      expect(properties[0].name).toBeDefined();
+      expect(properties[0].url).toBeDefined();
+    });
+
+    it("should create multiple bookmark nodes", async () => {
+      const query = `
+          UNWIND $sites AS site
+          MERGE (s:Site { url: site.url, userId: site.userId })
+          SET s = { name: site.name, url: site.url, userId: site.userId }
+          RETURN s;
+        `;
+
+      const sites = [
+        { name: "Github", url: "https://www.github.com", userId },
+        { name: "Stack Overflow", url: "https://stackoverflow.com", userId },
+        { name: "MDN", url: "https://developer.mozilla.org", userId }
+      ];
+
+      const res = await session.run(query, { sites });
+
+      const properties = res.records.map((record) => record.get(0).properties);
+
+      // Assert that 3 nodes are created
+      expect(properties.length).toBe(3);
+
+      // Check if each site's name and URL are defined
+      properties.forEach((site, index) => {
+        expect(site.name).toBeDefined();
+        expect(site.url).toBeDefined();
+        expect(site.name).toBe(sites[index].name);
+        expect(site.url).toBe(sites[index].url);
+      });
+    });
+
+    it("should update existing node with the same URL or create new one", async () => {
+      const query = `
+            MERGE (s:Site { url: $url, userId: $userId })
+            SET s = { url: $url, name: $name, userId: $userId }
+            RETURN s;
+        `;
+
+      const newName = "Github - Home";
+      const res = await session.run(query, {
+        name: newName,
+        url: "https://www.github.com",
+        userId
+      });
+
+      const properties = res.records.map((record) => record.get(0).properties);
+      expect(properties.length === 1, "should return one item");
+      expect(properties[0].name).toBe(newName);
+      expect(properties[0].url).toBe("https://www.github.com");
+
+      const searchQuery = `
+        MATCH (n:Site)
+        WHERE n.url = $url AND n.userId = $userId
+        RETURN n;
+      `;
+
+      const searchRes = await session.run(searchQuery, {
+        url: "https://www.github.com",
+        userId
+      });
+
+      const searchProps = searchRes.records.map(
+        (record) => record.get(0).properties
+      );
+
+      console.log(searchProps);
 
       expect(searchProps.length).toBe(1);
       expect(searchProps[0].name).toBe(newName);
