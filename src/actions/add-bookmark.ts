e4e8dcd469287d422eb4a4fe292c0d9c3c2a63a9
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-09-07 20:45:57 +0500

    feat add to database

--- src/actions/add-bookmark.ts
+++ src/actions/add-bookmark.ts
@@ -1,18 +1,78 @@
 "use server";
 
+import { connectNeo4j } from "@/lib/neo4j";
+import { delay } from "@/lib/utils";
+import * as cheerio from "cheerio";
+
 type FormState = {
-  message: string;
+  message?: string;
+  error?: string;
 };
 
 export default async function addBookMark(
   prevState: FormState,
   formData: FormData
 ) {
-  const url = formData.get("url");
+  const url = formData.get("url") as string;
 
-  await new Promise((r) => setTimeout(r, 3000));
+  if (!isValidURL(url)) return { error: "invalid url" };
+
+  const content = await Promise.race([fetchContent(url), delay(3000)]);
+
+  if (typeof content != "string") return { error: "Unable to fetch url" };
+
+  // fetch social cards
+  const $ = cheerio.load(content);
+
+  const ogCard = $(`head > [property="og:image"]`).attr("content") || "";
+  const title = $(`head > title`).text();
+  const description =
+    $(`head > meta[name="description"]`).attr("content") ||
+    "No description found.";
+
+  console.log(content, "og Card:", ogCard, title, " ", description);
+
+  // if([].every()) validate before
+
+  const { driver, session } = connectNeo4j();
+
+  const res = await session.run(
+    `CREATE (
+    s:Site 
+    { url: $url, ogCard: $ogCard, title: $title, description: $description }
+  ) RETURN s;`,
+    {
+      url,
+      ogCard,
+      title,
+      description
+    }
+  );
+
+  console.log(res.records.map((r) => r.get(0).properties));
 
   return {
     message: "hello world"
   };
 }
+
+const fetchContent = async (url: string) => {
+  const res = await (
+    await fetch(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+      }
+    })
+  ).text();
+  return res;
+};
+
+const isValidURL = (url: string) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
